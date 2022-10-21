b2d80046e2f3986b527a875b11a5506e13bf4e8d
Dawid Maroń <daw.maron@gmail.com>
2022-10-22 00:01:16 +0200

    create GUI class create GUI.showMenu

---
+++ src/GUI.java
@@ -0,0 +1,8 @@
+public class GUI {
+    private static String s1 = "1. Oblicz BMI\n";
+    private static String s2 = "2. Przypomnij BMI\n";
+    private static String s3 = "3. Exit\n";
+    public static void showMenu() {
+        System.out.println(s1+s2+s3);
+    }
+}
